03da46be2d817d1c3d9d37283a8b416298980c8e
Gaurav Gautam Shakya <grv.shakya0563@gmail.com>
2024-09-17 02:21:16 +0530

    preface added

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -7,17 +7,36 @@ function Research() {
       <h2> Problem Evolution to Super Intelligence</h2>
       <div className="research-container">
         <div className="research-mainarea">
-          <a>Topics</a>
+          <h3 style={{ textAlign: "center" }}>I ❤️ simple things not easy !</h3>
+          <p>
+            We, Human are pretty advanced, only species who managed to develop
+            Language, Technology, and complex social organization and has no competition on earth !{" "}
+          </p>
+          <a>Chapters</a>
           <a href="/preface">Preface</a>
           <a href="/intro">Introduction</a>
           <a href="/accident">One Accident, Invention or Discovery or what?</a>
-          <a href="/problem1">Type 1 - Problems: Intra-group Problems - People & Governance </a>
-          <a href="/problem2">Type 2 - Problems: Intra-group Problems - Internationalization</a>
-          <a href="/problem3">Type 3 - Problems: Cosmological & Environmental Wonders - The Religion</a>
-          <a href="/problem4">Type 4 - Problems: Hunam Problems - Agents of vulnerabilities, Love and compassion</a>
-          <a href="/abstract">Abstraction in Human problem space in current state of human regime</a>
+          <a href="/problem1">
+            Type 1 - Problems: Intra-group Problems - People & Governance{" "}
+          </a>
+          <a href="/problem2">
+            Type 2 - Problems: Intra-group Problems - Internationalization
+          </a>
+          <a href="/problem3">
+            Type 3 - Problems: Cosmological & Environmental Wonders - The
+            Religion
+          </a>
+          <a href="/problem4">
+            Type 4 - Problems: Hunam Problems - Agents of vulnerabilities, Love
+            and compassion
+          </a>
+          <a href="/abstract">
+            Abstraction in Human problem space in current state of human regime
+          </a>
           <a href="/conclusion">Conclusion</a>
-          <a href="/future">Future of Humanity</a>
+          <a href="/future">
+            Future and Current trend of Human Engineering and Evolution
+          </a>
         </div>
       </div>
     </>
--- src/pages/research-pages/Preface.js
+++ src/pages/research-pages/Preface.js
@@ -4,13 +4,258 @@ function Preface() {
   return (
     <>
       <div className="page-container">
-        
         <div className="research-mainarea">
-          <h2>Heading</h2>
-          In software engineering and computer science, abstraction is the
-          process of generalizing concrete details, such as attributes, away
-          from the study of objects and systems to focus attention on details of
-          greater importance
+          <h2>Preface</h2>
+          <p>Greetings!</p>
+          <p>Thank you for Coming!</p>
+          <h4>Who am I? </h4>
+          <p>
+            Hey, Reader. My name is Gaurav Gautam Shakya. I am an Engineer
+            otherwise Electrical engineer by academics for my family and society
+            because I pursued electrical engineering discipline at College. I
+            found that engineering( not restricted to electricity but the true
+            meaning of engineering) is the most fascinating career for me to
+            explore the possibilities of life beyond the social norms and
+            restrictions. Power of creation is the most powerful mechanism for
+            the realisation of the purpose of life and engineering unravels that
+            power to any Individual who devotes himself to the true meaning of
+            the word "the Engineering" beyond societal norms and standards.
+          </p>
+          <p>
+            Engineering is not meant to be piece of cake because creation is the
+            divine gift so very few people managed to make themselves eligible
+            for this gift across the world.
+          </p>
+          <h4>What is this book about and is it worth reading?</h4>
+          <p>
+            Humans are the only species on the earth so dominated that they do
+            not have any competition. Why is that so? How come humans became so
+            dominated and evolved in the Animal kingdom without competition?
+            There are many answers to this like humans are the only intelligent
+            species on the earth, only got reasoning capabilities, humans are
+            emotional beings, reason for domination is evolution and so on.
+          </p>
+          <p>
+            {" "}
+            Many people on the earth can not comprehend the complexity of social
+            enterprises such as family, marriage, intelligence and Emotional
+            intelligence, Government, Legal Framework, capital punishment,
+            system of exchange, healthcare system, learning and education,
+            ethics and morality, human rights, and many cosmological and
+            environmental wonders, disasters and calamities.{" "}
+          </p>
+          <p>
+            Of Course these are specialised domains of knowledge. one can only
+            get specialisation in one or two domains.
+          </p>
+          <p>
+            so, In this book, I tried to comprehend origins and reasons for
+            founding these complex domains of knowledge separate from others.
+            Based on these existential reasons, I tried to comprehend the
+            objectives and goals of these disciplines.
+          </p>
+          <p>
+            {" "}
+            The scope of incomprehensibly wide. So these are just my
+            understanding. Don't take me too seriously, have fun, and be curious
+            about the possibilities around you.
+          </p>
+          <p>
+            The reason for having such scope is that the problem I Interested in
+            has the same kind of scope. Content, Ideas and reasoning in this
+            book augmented the solution of the problem of my interest. The
+            problems is engineering super intelligence. Why super intelligence
+            and what value does it add to our life or is it for the BuzzFeed of
+            AI, general and Super Intelligence? That's great and profound
+            question. Let me cover some basics for you to understand my point of
+            view towards super intelligence. Before that I must confront that it
+            is not completely because of BuzzFeed. I choose to pursue this
+            career to engineer super intelligence because in technology,
+            especially computer engineering and Information Technology, every
+            subfield is overwhelmingly crowded and very narrow in scope. I was
+            looking for a new emerging and challenging field for myself.
+            Initially I hesitated because of buzzwords and sci-fi expectations
+            of people which did not attract me as an engineering discipline.
+            Accidentally, I started learning fuzzy logic and neural network as
+            my elective subject in my final year in college.
+          </p>
+          <p>
+            Eventually I fell in love with the scope and emerging, complex
+            nature. One advantage everyone gets in pioneering is that no-one
+            knows what works and what doesn't. This is the biggest advantage for
+            creating something very useful and revolutionary. With this thinking
+            I immersed myself in the study to find the solution for super
+            intelligence
+          </p>
+          <p>
+            I assume that you got the answer for the last question? Next comes,
+            why and How would I accomplish this grand Vision of solving
+            intelligence?
+          </p>
+          <p>Answer for why is</p>
+          <p>
+            Humans are very good at cognitive skills such as thinking,
+            imagination, reasoning and finding solutions to complex problems or
+            producing scientific explanations. There is a disparity between the
+            mental world and physical world. Mental world is agile, dynamic,
+            easily adaptable in any complex situation. On the other hand, the
+            physical world is full of frictions. It is very complex, hard
+            bounded with physical constraints and restrictions, Prolonged,
+            difficult to recover and reverse engineer, less likely to adapt in
+            most situations. These two worlds are very critical for our life to
+            accomplish balance between both. To survive and evolve further, it
+            is necessary to make balance both in terms of agility, adaptability
+            and consensus among natural constraints
+          </p>
+          <p>
+            My mental schematic for the Super Intelligence is to make sure the
+            mental world keeps evolving and becoming more and more advanced in
+            cognitive abilities and simultaneously the physical world must
+            eliminate the physical constraint and become more agile and
+            adaptable like the mental world. If that is possible with technology
+            and engineering that means that is the way to engineer super
+            Intelligence.
+          </p>
+          <h4>Why did I write this book? </h4>
+          <p>
+            I did not plan to write this book in the first place. I was
+            wondering around for the long time trying to understand the
+            environment around me, people and social enterprise which evolved as
+            pretty complex social organisation filled with social norms,
+            cultural diversity and disparity, stereotypes, vulnerabilities,
+            love, compassion, brutality, Learning, confusion, distraction,
+            Adolescence, self-loathing, hate, regret, trauma and depression and
+            so on and on.
+          </p>
+          <p>
+            Definition of these concepts and disciplines is not very much clear
+            and understood. The responsibility is given to a specialised system
+            of Education for defining these concepts and disciplines and
+            teaching others so that they can make good decisions, understand
+            people and cloud make harmony with existing society of systems and
+            can distinguish between good life and bad life. But unfortunately,
+            over the centuries and decades this responsibility got distracted
+            from the founding vision for having standalone systems for training
+            and education. Actors in the education system limit themselves to
+            just teaching generations for their professional competencies,
+            efficiency and effectiveness only in the legal enterprises such
+            government, corporations and industrial workplace. Very less focus
+            on the personal efficiency and effectiveness in social enterprises
+            such as family, marriage, personal relationship, self development,
+            social norms, diversity and disparity.
+          </p>
+          <p>
+            Learning styles, techniques and practices in the educational
+            settings does not have correlation with the natural learning
+            principles which promotes curiosity and constant persuasion of
+            learning, useful creations and improvement beyond scope knowledge.
+            There is reason to say that current educational settings do not
+            follow the principles of natural curiosity and internal drive and
+            hunger. If you visualise your life experience, you can easily point
+            out that the most important and profound learning you get always
+            comes through pain, uncertainty and discomfort. Barely you got any
+            useful learning without pain, discomfort and uncertainty. But
+            current institutions are trying hard to make education simple, easy
+            and fun. These efforts might be sincere and for Goodwill but they're
+            on the wrong track to make the education accessible to everyone but
+            not necessarily the education and learning needed by the students.
+          </p>
+          <p>These institutions are trying to do things.</p>
+          <ul>
+            <li>1. Education should be fun, fast and time constrained.</li>
+            <li>
+              2. Removing uncertainty by inserting hard bounded syllabus which
+              leaves no room for uncertainty and curiosity
+            </li>
+          </ul>
+          <p>Both things are against natural learning. </p>
+          <p>
+            When I spent an unjustified amount of time trying to learn something
+            useful by offshoring learning and development discretion to these
+            distracted institutions. Eventually I realised the kind of life I
+            want can not be achieved by offshoring the most precious discretion
+            for taking care of my learning and development. So I gathered some
+            courage and started my journey for self learning and training.
+            Responsibility of taking care of your own learning and development
+            is huge. Requires a lot of time, patience and determination.
+          </p>
+          <p>
+            When I started it was nightmare for me and very hard to get single
+            ray of sunshine what to do and where to begin. I wondered for 3 to 4
+            year before realising the reality and how big the the responsibility
+            I took for myself and why people usually look for teachers and
+            institutions. This experience is ineffable for me. That 4 years of
+            patience made me too curious about the World around me.
+            Consequently, I assigned myself the most difficult task, which is to
+            organise world knowledge with logic and reasoning.
+          </p>
+          <h4>Disclaimer</h4>
+          <p>
+            I tried to be rational and not to be biassed with my own beliefs and
+            stereotypes. But You are free to challenge these ideas. Even my
+            advice to you is that you must challenge these ideas and try to
+            improve your understanding. Do not take me too seriously. These are
+            logical prototype models for the human history of problem solving. I
+            simplified it to digest in one or two sittings and quickly reason
+            over the current major problems such as clean water, climate change,
+            incurable disease and their roots and importance in human history.
+          </p>
+          <p>
+            If you are an expert in any capacity and think my facts are not
+            scientifically and historically right and my way of consolidation of
+            humongous history and rationale of these ideas is not right in any
+            capacity. Yes, you can be right but I am just Engineer, that is why
+            I am biassed or trained myself to be biassed to abstractions,
+            reducing complexity and time, approximating problems to make useful
+            trade-offs
+          </p>
+          <h4>Why this book? </h4>
+          <p>
+            I am writing this book to understand the bigger circle of problems
+            and their importance in short as well as long term. We can also have
+            the understanding of human problems with their historical roots.
+          </p>
+          <p>
+            Many times I found myself hanging around the problem and facing
+            difficulty to understand the impact and importance of solution’s of
+            that problem which lead me to give up and move to look for new
+            problems because previous one look less important and less
+            impactful. But later I realised I have not developed any metal
+            framework through which I can scrutinise the problem and their
+            impact in the most effective and biggest context. Almost Every
+            problem on earth has its roots in history. If we have some framework
+            or body of Knowledge(not as exhaustive as Human History or
+            Evolutionary sciences or social science) which can give you the
+            bigger complete picture of Human problems and how they solve them.
+            What kind of problems are still around us invisible or
+            incomprehensible with today’s language, technology and our complex
+            social organisational understanding of humans. I am writing this
+            book to myself so that I can understand the bigger picture of
+            problems and be able to pick one or more urgent and impactful
+            problems to dedicate myself to solve
+          </p>
+          <p>
+            This book is not meant to be well scrutinised and correct facts
+            about the history of human evolution.
+          </p>
+          <h4>Use of this book, Value extraction from this book? </h4>
+          <p>
+            One possible use case could be that this book can be used to build a
+            logical picture of problems which have gone through in the past, are
+            facing in the present and will continue to face or will face
+            completely new, different problems in the future. For Building this
+            logical picture, you can use discretion to avoid going through all
+            human evolution.
+          </p>
+          <h4>Methodology used in this book</h4>
+          <p>
+            Methodology for this research is casual in nature. I will be
+            traversing the whole human evolution( only event of monumental
+            significance for humanity) by asking questions and answering the
+            causal relationship between the events that have happened in history
+            and their implications in the present.
+          </p>
+          <a href="/intro">Next Chapter</a>
         </div>
       </div>
     </>
